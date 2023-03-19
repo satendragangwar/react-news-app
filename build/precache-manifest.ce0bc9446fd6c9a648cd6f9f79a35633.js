f2ff56606e9135fe32b45bba0854e16bab6a07e4
satendra gangwar <101309019+satendragangwar@users.noreply.github.com>
2023-03-19 19:55:50 +0530

    Add files via upload

---
+++ build/precache-manifest.ce0bc9446fd6c9a648cd6f9f79a35633.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "72732c9e3864d9b1fcf6",
+    "url": "/static/css/main.5479ae81.chunk.css"
+  },
+  {
+    "revision": "72732c9e3864d9b1fcf6",
+    "url": "/static/js/main.72732c9e.chunk.js"
+  },
+  {
+    "revision": "2c80ac1fe2aa0d16567f",
+    "url": "/static/js/1.2c80ac1f.chunk.js"
+  },
+  {
+    "revision": "c92bb1d7e958d73a2e2d",
+    "url": "/static/js/2.c92bb1d7.chunk.js"
+  },
+  {
+    "revision": "b7a5294dd25a4e7d9fd9",
+    "url": "/static/js/runtime~main.b7a5294d.js"
+  },
+  {
+    "revision": "6f02b542e433cc375b2d5a0fdc231150",
+    "url": "/static/media/loading.6f02b542.gif"
+  },
+  {
+    "revision": "6d91225115a8cc7d986f5e126f7c2c6c",
+    "url": "/index.html"
+  }
+];
